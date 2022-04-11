8010b1b5e23fff94cdf2eb7dab7a4ad0025231c1
indexpro24@gmail.com <indexpro24@gmail.com>
2022-04-11 18:05:46 +0300

    git init

--- app/src/lib/Footer/index.svelte
+++ app/src/lib/Footer/index.svelte
@@ -1,1 +1,14 @@
-<p>Футер</p>
+<script>
+    import Footer from "$lib/components/footer/v_001/index.svelte";
+    import { useFooter } from "$lib/components/footer/content";
+    const { menu, text, dataStart, option } = useFooter;
+    const dataFooter = {
+        option,
+        menu,
+        text,
+        dataStart
+    }
+
+</script>
+
+<Footer {...dataFooter}/>
---
+++ app/src/lib/components/footer/content/index.js
@@ -0,0 +1,23 @@
+const option = '1'
+const menu = [
+    {
+        id: 1,
+        value: 'Главная',
+        path: '/',
+    },
+    {
+        id: 2,
+        value: 'О нас',
+        path: '/about',
+    },
+]
+
+const text = 'Компания "Новострой". Ремонт квартир в Дзержинске любой сложности.'
+const dataStart = '2015'
+
+export const useFooter = {
+    option,
+    menu,
+    text,
+    dataStart
+}
