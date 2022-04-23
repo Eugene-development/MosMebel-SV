8088c37adb6200a748ac7873a0d449ac08fb2fd2
indexpro24@gmail.com <indexpro24@gmail.com>
2022-04-23 21:02:36 +0300

    git init

---
+++ app/src/brocker/index.js
@@ -0,0 +1,1 @@
+export { default as Info_v_001 } from '$lib/components/info/v_001/index.svelte'
--- app/src/lib/layouts/Header/v_001/index.svelte
+++ app/src/lib/layouts/Header/v_001/index.svelte
@@ -1,5 +1,7 @@
 <script>
-    import Information from '$lib/components/info/v_001/index.svelte'
+
+    import {Info_v_001} from "../../../../brocker/index.js";
+    // import Information from '$lib/components/info/v_001/index.svelte'
     import { useInfo } from "$lib/components/info/content/index.js";
     const { Info } = useInfo
     const dataInfo = {
@@ -8,4 +10,4 @@
 
 </script>
 
-<Information { ...dataInfo }/>
+<Info_v_001 { ...dataInfo }/>
