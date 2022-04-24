11d3dbd1655005ca61f807147d1e59f874c4f0dd
indexpro24@gmail.com <indexpro24@gmail.com>
2022-04-24 18:25:48 +0300

    git init

--- app/src/lib/brocker/index.js
+++ app/src/lib/brocker/index.js
@@ -1,2 +1,4 @@
 export { default as Info_v_001 } from '$lib/components/info/v_001/index.svelte'
 export { default as Info_v_002 } from '$lib/components/info/v_002/index.svelte'
+
+export { default as Main_v_001 } from '$lib/components/main/v_001/index.svelte'
--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { Main_v_001} from "$lib/brocker/index.js";
+
 	const title = 'Кухонные гарнитуры и шкафы купе в Москве и области'
 	const description = 'Предлагаем шкафы купе и кухонные гарнитуры стандартного размера и под заказ в Москве и области'
 </script>
@@ -8,129 +10,7 @@
 	<meta name="description" content="{description}">
 </svelte:head>
 
-<div class="relative bg-gray-800 overflow-hidden">
-	<div class="hidden sm:block sm:absolute sm:inset-0" aria-hidden="true">
-		<svg class="absolute bottom-0 right-0 transform translate-x-1/2 mb-48 text-gray-700 lg:top-0 lg:mt-28 lg:mb-0 xl:transform-none xl:translate-x-0" width="364" height="384" viewBox="0 0 364 384" fill="none">
-			<defs>
-				<pattern id="eab71dd9-9d7a-47bd-8044-256344ee00d0" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-					<rect x="0" y="0" width="4" height="4" fill="currentColor" />
-				</pattern>
-			</defs>
-			<rect width="364" height="384" fill="url(#eab71dd9-9d7a-47bd-8044-256344ee00d0)" />
-		</svg>
-	</div>
-	<div class="relative pt-6 pb-16 sm:pb-24">
-
-		<main class="mt-8 sm:mt-12">
-			<div class="mx-auto max-w-7xl">
-				<div class="lg:grid lg:grid-cols-12 lg:gap-28">
-					<div class="px-4 sm:px-6 sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left lg:flex lg:items-center">
-						<div>
-							<a href="#" class="inline-flex items-center text-white bg-gray-900 rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200">
-								<span class="ml-0.5 px-4 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-green-500 to-cyan-600 rounded-full">Мы открыты</span>
-								<span class="ml-4 p-1 text-sm">Посетите наши офисы</span>
-								<!-- Heroicon name: solid/chevron-right -->
-								<svg class="ml-2 w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-									<path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-								</svg>
-							</a>
-							<h1 class="mt-4 text-3xl tracking-tight font-extrabold text-white sm:mt-5 sm:leading-none lg:mt-6 lg:text-4xl xl:text-5xl">
-								<span class="md:block">Шкафы купе и <br> кухонные гарнитуры</span>
-								<span class="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-green-200 to-cyan-400 sm:pb-5">под заказ <br> от производителей</span>
-							</h1>
-							<p class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua ad ad non deserunt sunt.</p>
-							<p class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua ad ad non deserunt sunt.</p>
-<!--							<p class="mt-8 text-sm text-white uppercase tracking-wide font-semibold sm:mt-10">Наши партнёры</p>-->
-							<div class="mt-5 w-full sm:mx-auto sm:max-w-lg lg:ml-0">
-								<div class="flex flex-wrap items-start justify-between">
-									<div class="flex justify-center px-1">
-										<img class="h-9 sm:h-10" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple">
-									</div>
-									<div class="flex justify-center px-1">
-										<img class="h-9 sm:h-10" src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg" alt="Workcation">
-									</div>
-									<div class="flex justify-center px-1">
-										<img class="h-9 sm:h-10" src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg" alt="StaticKit">
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="mt-16 sm:mt-24 lg:mt-0 lg:col-span-6">
-						<div class="bg-white sm:max-w-md sm:w-full sm:mx-auto sm:rounded-lg sm:overflow-hidden">
-							<div class="px-4 py-8 sm:px-10">
-								<div>
-									<p class="text-lg font-bold text-gray-700 text-center ">Экспресс заявка на консультацию</p>
-
-								</div>
-
-								<div class="mt-6 relative">
-									<div class="absolute inset-0 flex items-center" aria-hidden="true">
-										<div class="w-full border-t border-gray-300"></div>
-									</div>
-									<div class="relative flex justify-center text-sm">
-										<span class="px-2 bg-white text-gray-500 "> бесплатно </span>
-									</div>
-								</div>
-
-								<div class="mt-6">
-									<form action="#" method="POST" class="space-y-6">
-										<div>
-											<label for="name" class="sr-only">Имя</label>
-											<input type="text" name="name" id="name" autocomplete="name" placeholder="Ваше имя" required class="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md">
-										</div>
-
-										<div>
-											<label for="mobile-or-email" class="sr-only">Укажите телефон</label>
-											<input type="text" name="mobile-or-email" id="mobile-or-email" autocomplete="phone" placeholder="Укажите телефон" required class="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md">
-										</div>
-
-										<div>
-											<label for="object" class="sr-only">Адрес объекта</label>
-											<input type="text"  name="password" id="object" autocomplete="object" placeholder="Адрес объекта" required class="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md">
-										</div>
-
-										<div>
-											<label for="time" class="sr-only">Удобное время</label>
-											<input type="text" name="password" id="time" autocomplete="time" placeholder="Удобное время" required class="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md">
-										</div>
-
-										<div>
-											<button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gradient-to-r from-green-300 hover:from-green-200 via-cyan-600 hover:via-cyan-700 to-green-300 hover:to-green-200 ">Отправить</button>
-										</div>
-									</form>
-								</div>
-							</div>
-							<div class="px-4 py-6 bg-gray-50 border-t-2 border-gray-200 sm:px-10">
-								<p class="text-xs leading-5 text-gray-500 text-center">После отправки заявки с вами свяжется менеджер <strong>Евгений</strong> для уточнения нюансов</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</main>
-	</div>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<Main_v_001/>
 
 <div class="">
 	<div class="max-w-2xl mx-auto px-4 py-8 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8">
