0d8baf3757f38a6fc3ae9765c6a1b306b17ac794
indexpro24@gmail.com <indexpro24@gmail.com>
2022-04-24 17:20:38 +0300

    git init

--- app/src/lib/layouts/Header/v_001/index.svelte
+++ app/src/lib/layouts/Header/v_001/index.svelte
@@ -44,8 +44,16 @@
                 <a href="/contacts" class="font-medium text-white hover:text-gray-300">Контакты</a>
             </div>
         </div>
-        <div class="hidden md:flex">
-            <a href="tel:+74951475550" class="inline-flex items-center px-4 py-2 border border-transparent text-lg font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"> +7 (495) 147-55-50 </a>
+        <div class="hidden lg:flex items-center px-2">
+            <div class="">
+                <div class="flex space-x-2 bg-gray-700 hover:bg-gray-600 px-3 py-2 rounded-xl">
+                    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-gray-50" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 3l-6 6m0 0V4m0 5h5M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
+                    </svg>
+                    <a href="tel:+74951475550" class="text-gray-50 rounded-md text-xl font-bold ">+7 (495) 147-55-50</a>
+                </div>
+            </div>
         </div>
     </nav>
 
