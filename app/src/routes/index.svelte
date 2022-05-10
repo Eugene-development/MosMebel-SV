6a8a595ad2282999997a209a74fcda1ca2c616aa
indexpro24@gmail.com <indexpro24@gmail.com>
2022-05-10 18:30:00 +0300

    git init

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -161,7 +161,7 @@
 <div class="py-16 bg-gray-50 overflow-hidden">
 	<div class="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
 		<div class="text-base max-w-prose mx-auto lg:max-w-none">
-			<h2 class="text-base text-main font-semibold tracking-wide uppercase">Компания</h2>
+			<h2 class="text-base text-main font-semibold tracking-wide uppercase">Мебельная компания</h2>
 			<p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">"Мос-Мебель"</p>
 		</div>
 		<div class="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-start">
@@ -199,12 +199,11 @@
 				</svg>
 
 
-				{#each MainTestimonials as { name, text }}
-				<blockquote class="mb-8 relative bg-white rounded-lg shadow-lg">
+				{#each MainTestimonials as { name, text, img }}
+				<blockquote class="mb-16 relative bg-white rounded-lg shadow-lg">
 					<div class="rounded-t-lg px-6 py-8 sm:px-10 sm:pt-10 sm:pb-8 ">
-
 						<div class="relative text-lg text-gray-700 font-medium mt-8">
-							<svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-gray-200" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
+							<svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-gray-300" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
 								<path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
 							</svg>
 							<p class="relative">{ text }</p>
@@ -212,7 +211,7 @@
 					</div>
 					<div class="relative flex items-center sm:items-start bg-gray-800 rounded-b-lg not-italic py-5 px-6 sm:py-5 sm:pl-12 sm:pr-10 sm:mt-10">
 						<div class="relative rounded-full border-2 border-white sm:absolute sm:top-0 sm:transform sm:-translate-y-1/2">
-							<img class="w-12 h-12 sm:w-20 sm:h-20 rounded-full bg-main" src="https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2.5&w=160&h=160&q=80" alt="">
+							<img class="w-12 h-12 sm:w-20 sm:h-20 rounded-full bg-main" src="{img}" alt="фото клиента">
 						</div>
 						<div class="relative ml-4 text-main font-semibold leading-6 sm:ml-24 sm:pl-1">
 						  <p class="text-white font-semibold sm:inline">{ name }</p>
