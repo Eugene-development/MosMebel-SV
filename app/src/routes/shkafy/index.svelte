7be80b783bb293399bb540e452b7472123cce578
indexpro24@gmail.com <indexpro24@gmail.com>
2022-05-10 19:45:20 +0300

    git init

--- app/src/routes/shkafy/index.svelte
+++ app/src/routes/shkafy/index.svelte
@@ -13,37 +13,37 @@
     <div class="max-w-3xl mx-auto py-24 px-4 grid items-center grid-cols-1 gap-y-16 gap-x-8 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
         <div>
             <h2 class="max-w-3xl mx-auto text-4xl tracking-tight font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 via-red-700 to-blue-700 sm:text-5xl">Распашные шкафы и шкафы-купе недорого и качественно</h2>
-            <p class="mt-4 text-gray-500">Недорогие шкафы купе по низким ценам</p>
+            <p class="mt-4 text-gray-500">Недорогие шкафы купе по низким ценам, а также распашные и встроенные. Под ваш размер в комнату или прихожую.</p>
 
             <dl class="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
                 <div class="border-t border-gray-200 pt-4">
                     <dt class="font-medium text-gray-900">Шкафы купе</dt>
-                    <dd class="mt-2 text-sm text-gray-500">Designed by Good Goods, Inc.</dd>
+                    <dd class="mt-2 text-sm text-gray-500">Подвесная и нижнеопорная система высокого качества</dd>
                 </div>
 
                 <div class="border-t border-gray-200 pt-4">
                     <dt class="font-medium text-gray-900">Распашные</dt>
-                    <dd class="mt-2 text-sm text-gray-500">Solid walnut base with rare earth magnets and powder coated steel card cover</dd>
+                    <dd class="mt-2 text-sm text-gray-500">Двух, трёх и четырёх створчатые с выдвижными ящиками</dd>
                 </div>
 
                 <div class="border-t border-gray-200 pt-4">
                     <dt class="font-medium text-gray-900">Встроенные</dt>
-                    <dd class="mt-2 text-sm text-gray-500">6.25&quot; x 3.55&quot; x 1.15&quot;</dd>
+                    <dd class="mt-2 text-sm text-gray-500">Безкаркасные со встраиваемыми полками влитые в нишу</dd>
                 </div>
 
                 <div class="border-t border-gray-200 pt-4">
                     <dt class="font-medium text-gray-900">Гардеробные</dt>
-                    <dd class="mt-2 text-sm text-gray-500">Hand sanded and finished with natural oil</dd>
+                    <dd class="mt-2 text-sm text-gray-500">Полнофункциональные гардеробные с любым наполнением</dd>
                 </div>
 
                 <div class="border-t border-gray-200 pt-4">
                     <dt class="font-medium text-gray-900">Двери купе</dt>
-                    <dd class="mt-2 text-sm text-gray-500">Wood card tray and 3 refill packs</dd>
+                    <dd class="mt-2 text-sm text-gray-500">В качестве перегородок или для замены старых дверей</dd>
                 </div>
 
                 <div class="border-t border-gray-200 pt-4">
                     <dt class="font-medium text-gray-900">Офисные</dt>
-                    <dd class="mt-2 text-sm text-gray-500">Made from natural materials. Grain and color vary with each item.</dd>
+                    <dd class="mt-2 text-sm text-gray-500">Стильные и строгие варианты шкафов для офисных помещений</dd>
                 </div>
             </dl>
         </div>
