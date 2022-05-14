821bd3ffa46115b816135ce45381ce18a435c7b6
indexpro24@gmail.com <indexpro24@gmail.com>
2022-05-14 14:59:07 +0300

    git init

---
+++ app/src/routes/post/1/index.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let title;
+    export let description;
+    export let text;
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+    <meta name="description" content="{description}">
+</svelte:head>
+
+{@html text}
