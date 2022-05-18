31b4d40db52639b96c5fe34809653965d3557bcf
indexpro24@gmail.com <indexpro24@gmail.com>
2022-05-18 21:37:26 +0300

    git init

--- app/src/lib/brocker/index.js
+++ app/src/lib/brocker/index.js
@@ -5,8 +5,8 @@ export { default as Main_v_001 } from '$lib/components/main/v_001/index.svelte'
 
 export { default as Panel_v_001 } from '$lib/components/marketing/panel/v_001/index.svelte'
 
-export { default as SectionV1 } from '$lib/components/marketing/sectionV1/v_001/index.svelte'
-export { default as SectionV2 } from '$lib/components/marketing/sectionV2/v_001/index.svelte'
-export { default as SectionV3 } from '$lib/components/marketing/sectionV3/v_001/index.svelte'
+export { default as MarketingSectionV1 } from '$lib/components/marketing/sectionV1/v_001/index.svelte'
+export { default as MarketingSectionV2 } from '$lib/components/marketing/sectionV2/v_001/index.svelte'
+export { default as MarketingSectionV3 } from '$lib/components/marketing/sectionV3/v_001/index.svelte'
 
 export { default as BlogSectionV1 } from '$lib/components/blog/sectionV1/v_001/index.svelte'
--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,5 +1,11 @@
 <script>
-	import { Main_v_001, SectionV1, SectionV2, SectionV3, BlogSectionV1 } from "$lib/brocker/index.js";
+	import {
+		Main_v_001,
+		MarketingSectionV1,
+		MarketingSectionV2,
+		MarketingSectionV3,
+		BlogSectionV1,
+	} from "$lib/brocker/index.js";
 	export let title;
 	export let description;
 	export let contentSectionV1;
@@ -12,8 +18,8 @@
 </svelte:head>
 
 <Main_v_001/>
-<SectionV1 {...contentSectionV1}/>
+<MarketingSectionV1 {...contentSectionV1}/>
 <BlogSectionV1/>
-<SectionV2/>
-<SectionV3 {...contentSectionV3}/>
+<MarketingSectionV2/>
+<MarketingSectionV3 {...contentSectionV3}/>
 
