d0192f186db3cb55f59a5947236e601f086d5c51
indexpro24@gmail.com <indexpro24@gmail.com>
2022-05-19 13:02:50 +0300

    git init

--- app/src/routes/shkafy/index.svelte
+++ app/src/routes/shkafy/index.svelte
@@ -8,12 +8,88 @@
     <meta name="description" content="{description}">
 </svelte:head>
 
+
 <!-- This example requires Tailwind CSS v2.0+ -->
-<div class="bg-white">
+<!--<div class="bg-gray-800">-->
+<!--    <div class="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">-->
+<!--        <h2 class="text-3xl font-extrabold text-white sm:text-4xl">-->
+<!--            <span class="block">Boost your productivity.</span>-->
+<!--            <span class="block">Start using Workflow today.</span>-->
+<!--        </h2>-->
+<!--        <p class="mt-4 text-lg leading-6 text-indigo-200">Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla nec.</p>-->
+<!--        <a href="#" class="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50 sm:w-auto"> Sign up for free </a>-->
+<!--    </div>-->
+<!--</div>-->
+
+
+
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="relative bg-gray-700">
+    <div class="absolute inset-0">
+        <img class="w-full h-full object-cover" src="https://i.siteapi.org/vXeK2C7o83-hUmIDMilznLkQqKA=/fit-in/330x/top/filters:format(webp)/d03f5c6ef4e0d96.s.siteapi.org/img/d60c0cf34ce63fcd8b9e01d55efda97cbbeb2c17.JPG" alt="">
+        <div class="absolute inset-0 bg-gray-500 mix-blend-multiply" aria-hidden="true"></div>
+    </div>
+    <div class="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
+        <h1 class="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">Шкафы купе в Москве</h1>
+        <p class="mt-6 text-xl text-indigo-100 max-w-3xl">Mattis amet hendrerit dolor, quisque lorem pharetra. Pellentesque lacus nisi urna, arcu sociis eu. Orci vel lectus nisl eget eget ut consectetur. Sit justo viverra non adipisicing elit distinctio.</p>
+    </div>
+</div>
+
+
+
+<!-- This example requires Tailwind CSS v2.0+ -->
+<!--<div class="relative bg-slate-50 overflow-hidden">-->
+<!--    <div class="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full" aria-hidden="true">-->
+<!--        <div class="relative h-full max-w-7xl mx-auto">-->
+<!--            <svg class="absolute right-full transform translate-y-1/4 translate-x-1/4 lg:translate-x-1/2" width="404" height="784" fill="none" viewBox="0 0 404 784">-->
+<!--                <defs>-->
+<!--                    <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">-->
+<!--                        <rect x="0" y="0" width="4" height="4" class="text-gray-300" fill="currentColor" />-->
+<!--                    </pattern>-->
+<!--                </defs>-->
+<!--                <rect width="404" height="784" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />-->
+<!--            </svg>-->
+<!--            <svg class="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2" width="404" height="784" fill="none" viewBox="0 0 404 784">-->
+<!--                <defs>-->
+<!--                    <pattern id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">-->
+<!--                        <rect x="0" y="0" width="4" height="4" class="text-gray-300" fill="currentColor" />-->
+<!--                    </pattern>-->
+<!--                </defs>-->
+<!--                <rect width="404" height="784" fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)" />-->
+<!--            </svg>-->
+<!--        </div>-->
+<!--    </div>-->
+
+<!--    <div class="relative pt-6 pb-16 sm:pb-24">-->
+<!--        <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">-->
+<!--            <div class="text-center">-->
+<!--                <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">-->
+<!--                    <span class="block xl:inline">Шкафы купе</span>-->
+<!--                    <span class="block text-teal-900 xl:inline">в Москве</span>-->
+<!--                </h1>-->
+<!--                <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>-->
+<!--                <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">-->
+<!--                    <div class="rounded-md shadow">-->
+<!--                        <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-teal-900 hover:bg-teal-700 md:py-4 md:text-lg md:px-10"> Каталог </a>-->
+<!--                    </div>-->
+<!--                    <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">-->
+<!--                        <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-teal-900 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"> Консультация </a>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </main>-->
+<!--    </div>-->
+<!--</div>-->
+
+
+
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="bg-gradient-to-b from-white via-white to-gray-50">
     <div class="max-w-3xl mx-auto py-24 px-4 grid items-center grid-cols-1 gap-y-16 gap-x-8 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
         <div>
+<!--            <h2 class="max-w-3xl mx-auto text-4xl tracking-tight font-extrabold text-gray-800 sm:text-4xl">Распашные шкафы и шкафы купе недорого и качественно</h2>-->
             <h2 class="max-w-3xl mx-auto text-4xl tracking-tight font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 via-red-700 to-blue-700 sm:text-4xl">Распашные шкафы и шкафы купе недорого и качественно</h2>
-            <p class="mt-4 text-gray-500">Недорогие шкафы купе по низким ценам, а также распашные и встроенные. Под ваш размер в комнату или прихожую.</p>
+            <p class="mt-4 text-gray-500">Предлагаем недорогие шкафы купе по низким ценам, а также распашные и встроенные. Под ваш размер в комнату или прихожую.</p>
 
             <dl class="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
                 <div class="border-t border-gray-200 pt-4">
@@ -50,21 +126,28 @@
         <div class="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-4 lg:gap-6">
             <img alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
                  class="bg-gray-100 rounded-lg"
-                 src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg">
+                 src="https://i.siteapi.org/vXeK2C7o83-hUmIDMilznLkQqKA=/fit-in/330x/top/filters:format(webp)/d03f5c6ef4e0d96.s.siteapi.org/img/d60c0cf34ce63fcd8b9e01d55efda97cbbeb2c17.JPG">
             <img alt="Top down view of walnut card tray with embedded magnets and card groove."
                  class="bg-gray-100 rounded-lg"
-                 src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg">
+                 src="https://i.siteapi.org/vXeK2C7o83-hUmIDMilznLkQqKA=/fit-in/330x/top/filters:format(webp)/d03f5c6ef4e0d96.s.siteapi.org/img/d60c0cf34ce63fcd8b9e01d55efda97cbbeb2c17.JPG">
             <img alt="Side of walnut card tray with card groove and recessed card area."
-                 class="bg-gray-100 rounded-lg" src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg">
+                 class="bg-gray-100 rounded-lg"
+                 src="https://i.siteapi.org/vXeK2C7o83-hUmIDMilznLkQqKA=/fit-in/330x/top/filters:format(webp)/d03f5c6ef4e0d96.s.siteapi.org/img/d60c0cf34ce63fcd8b9e01d55efda97cbbeb2c17.JPG">
             <img alt="Walnut card tray filled with cards and card angled in dedicated groove."
                  class="bg-gray-100 rounded-lg"
-                 src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg">
+                 src="https://i.siteapi.org/vXeK2C7o83-hUmIDMilznLkQqKA=/fit-in/330x/top/filters:format(webp)/d03f5c6ef4e0d96.s.siteapi.org/img/d60c0cf34ce63fcd8b9e01d55efda97cbbeb2c17.JPG">
+<!--            <img alt="Side of walnut card tray with card groove and recessed card area."-->
+<!--                 class="bg-gray-100 rounded-lg"-->
+<!--                 src="https://i.siteapi.org/vXeK2C7o83-hUmIDMilznLkQqKA=/fit-in/330x/top/filters:format(webp)/d03f5c6ef4e0d96.s.siteapi.org/img/d60c0cf34ce63fcd8b9e01d55efda97cbbeb2c17.JPG">-->
+<!--            <img alt="Walnut card tray filled with cards and card angled in dedicated groove."-->
+<!--                 class="bg-gray-100 rounded-lg"-->
+<!--                 src="https://i.siteapi.org/vXeK2C7o83-hUmIDMilznLkQqKA=/fit-in/330x/top/filters:format(webp)/d03f5c6ef4e0d96.s.siteapi.org/img/d60c0cf34ce63fcd8b9e01d55efda97cbbeb2c17.JPG">-->
         </div>
     </div>
 </div>
 
 
-<div class="relative py-16 bg-white overflow-hidden">
+<div class="relative py-16 bg-gradient-to-b from-white via-white to-gray-50 overflow-hidden">
     <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
         <div aria-hidden="true" class="relative h-full text-lg max-w-prose mx-auto">
             <svg class="absolute top-12 left-full transform translate-x-32" fill="none" height="384"
