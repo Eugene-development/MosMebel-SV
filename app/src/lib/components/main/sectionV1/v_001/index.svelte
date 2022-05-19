33a5bb21e28fd21d715dfc98ec7dfe89f5d13ebd
indexpro24@gmail.com <indexpro24@gmail.com>
2022-05-19 20:45:51 +0300

    git init

--- app/src/lib/components/main/sectionV1/v_001/index.svelte
+++ app/src/lib/components/main/sectionV1/v_001/index.svelte
@@ -1,6 +1,7 @@
 <script>
     import {gsap} from 'gsap'
     import {onMount} from 'svelte';
+
     onMount(async () => {
         // gsap.to("#b", {duration: 4, x: 200, opacity: 0.3});
 
@@ -10,6 +11,8 @@
             duration,
             opacity: 0
         })
+            .to("#box1", {duration: 1, x:10})
+            .to("#box2", {duration: 1, x:10})
         // tl.from('#b2', {
         //     duration,
         //     opacity: 0
@@ -44,24 +47,38 @@
                                 <span class="ml-0.5 px-4 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-green-500 to-cyan-600 rounded-md">Мы открыты</span>
                                 <span class="ml-4 p-1 text-sm">Посетите наши офисы</span>
                                 <!-- Heroicon name: solid/chevron-right -->
-                                <svg class="ml-2 w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                                <svg aria-hidden="true" class="ml-2 w-5 h-5 text-gray-500"
+                                     fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path clip-rule="evenodd"
+                                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                          fill-rule="evenodd"/>
                                 </svg>
                             </a>
                             <h1 class="mt-4  tracking-tight font-extrabold text-white sm:mt-5 sm:leading-none lg:mt-6 text-4xl lg:text-4xl xl:text-5xl antialiased">
                                 <span class="md:block">Шкафы купе и <br> кухонные гарнитуры</span>
                                 <span class="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-green-200 to-cyan-400 sm:pb-5">под заказ <br> от производителей</span>
                             </h1>
-                            <p  class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-base lg:text-base xl:text-base antialiased">Шкафы купе по низким ценам и с разными вариантами установки дверей. Вы можете заказать у нас как шкаф эконом сегмента так и премиальный вариант. Также вы можете приобрести только внутреннее наполнение или сами двери купе.</p>
-                            <p class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-base lg:text-base xl:text-base antialiased">Если вы хотите приобрести кухонный гарнитур недорого и с высоким качеством, то предлагаем сделать это у нас. Мы предоставим оптимальное соотношение качества и цены на кухонные гарнитуры вне зависимости от ценовой категории.</p>
+                            <p class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-base lg:text-base xl:text-base antialiased"
+                               id="box1">
+                                Шкафы купе по низким ценам и с разными вариантами установки дверей. Вы можете заказать у
+                                нас как шкаф эконом сегмента так и премиальный вариант. Также вы можете приобрести
+                                только внутреннее наполнение или сами двери купе.</p>
+                            <p class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-base lg:text-base xl:text-base antialiased"
+                               id="box2">
+                                Если вы хотите приобрести кухонный гарнитур недорого и с высоким качеством, то
+                                предлагаем сделать это у нас. Мы предоставим оптимальное соотношение качества и цены на
+                                кухонные гарнитуры вне зависимости от ценовой категории.</p>
                             <!--							<p class="mt-8 text-sm text-white uppercase tracking-wide font-semibold sm:mt-10">Наши партнёры</p>-->
                             <div class="mt-5 w-full sm:mx-auto sm:max-w-lg lg:ml-0">
                                 <div class="flex flex-wrap items-start justify-between">
                                     <div class="flex justify-center px-1">
-                                        <img class="h-9 sm:h-10" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple">
+                                        <img alt="Tuple"
+                                             class="h-9 sm:h-10" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg">
                                     </div>
                                     <div class="flex justify-center px-1">
-                                        <img class="h-9 sm:h-10" src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg" alt="Workcation">
+                                        <img alt="Workcation"
+                                             class="h-9 sm:h-10"
+                                             src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg">
                                     </div>
                                     <div class="flex justify-center px-1">
                                         <img class="h-9 sm:h-10" src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg" alt="StaticKit">
