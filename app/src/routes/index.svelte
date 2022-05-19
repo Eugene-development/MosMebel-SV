17dad01530f56941cbcaeb3522f10824fdcef204
indexpro24@gmail.com <indexpro24@gmail.com>
2022-05-19 13:42:01 +0300

    git init

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,4 +1,27 @@
 <script>
+	import { gsap } from 'gsap'
+
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+
+		console.log("loaded gsap onMount")
+
+		const tl = gsap.timeline();
+		const duration = 1;
+
+		tl.from('.box', {
+			duration,
+			opacity: 0
+		})
+				.from('.box', {
+					duration,
+					xPercent: 100,
+					rotation: -90,
+					yPercent: 100,
+					ease: 'bounce.out',
+				}, `-=${duration * 0.75}`)
+	});
 	import {
 		MainSectionV1,
 		MarketingSectionV1,
@@ -17,9 +40,18 @@
 	<meta name="description" content="{description}">
 </svelte:head>
 
+<div class="box"></div>
+
 <MainSectionV1/>
 <MarketingSectionV1 {...contentSectionV1}/>
 <BlogSectionV1/>
 <MarketingSectionV2/>
 <MarketingSectionV3 {...contentSectionV3}/>
 
+<style>
+	.box {
+		width: 100px;
+		height: 100px;
+		background: black;
+	}
+</style>
