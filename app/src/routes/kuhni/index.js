3f2516470a0e418fc46952ebda77dcc222dcee1e
indexpro24@gmail.com <indexpro24@gmail.com>
2022-05-22 17:16:36 +0300

    git init

--- app/src/routes/kuhni/index.js
+++ app/src/routes/kuhni/index.js
@@ -6,12 +6,80 @@ const dataMarketingSectionV4 = {
     description: 'Кухонные гарнитуры с учётом всех нюансов вашего помещения и пожеланий',
 }
 
+const mainText =
+    {
+        title: 'Кухонные гарнитуры на заказ и по стандартным модулям',
+        description: 'Недорогие кухонные гарнитуры по низким ценам. Выбирайте наиболее подходящий под ваши нужды вариант.'
+    }
+
+const img = [
+    {
+        path: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/1.jpg',
+        alt: ''
+    },
+    {
+        path: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/9.JPG',
+        alt: ''
+    },
+    {
+        path: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/3.jpg',
+        alt: ''
+    },
+    {
+        path: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/5.jpg',
+        alt: ''
+    },
+    {
+        path: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/6.jpg',
+        alt: ''
+    },
+    {
+        path: 'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/7.jpg',
+        alt: ''
+    },
+]
+
+const types = [
+    {
+        title: 'Прямые',
+        description: 'Прямые линейные кухонные гарнитуры во всю стену'
+    },
+    {
+        title: 'Угловые',
+        description: 'Угловые кухонные гарнитуры на один или два угла'
+    },
+    {
+        title: 'Встроенные',
+        description: 'Встроенные кухонные гарнитуры от пола до потолка'
+    },
+    {
+        title: 'Модульные',
+        description: 'Модули кухонных гарнитуров, которые вам нужны'
+    },
+    {
+        title: 'Пластик',
+        description: 'Кухонные гарнитуры с фасадами из пластика в профиле'
+    },
+    {
+        title: 'МДФ ПВХ',
+        description: 'Кухонные гарнитуры с фасадами из МДФ с покрытием ПВХ'
+    },
+]
+
+const contentMarketingSectionV5 = {
+    mainText,
+    img,
+    types
+}
+
+
 export const get = () => {
     return {
         body : {
             seoTitle,
             seoDescription,
-            dataMarketingSectionV4
+            dataMarketingSectionV4,
+            contentMarketingSectionV5
         }
     }
 }
--- app/src/routes/kuhni/index.svelte
+++ app/src/routes/kuhni/index.svelte
@@ -1,9 +1,10 @@
 <script>
-    import {MarketingSectionV4} from "$lib/brocker/index.js";
+    import {MarketingSectionV4, MarketingSectionV5} from "$lib/brocker";
 
     export let seoTitle;
     export let seoDescription;
     export let dataMarketingSectionV4;
+    export let contentMarketingSectionV5;
 </script>
 
 <svelte:head>
@@ -12,63 +13,7 @@
 </svelte:head>
 
 <MarketingSectionV4 {...dataMarketingSectionV4}/>
-
-<!-- This example requires Tailwind CSS v2.0+ -->
-<div class="bg-white">
-    <div class="max-w-2xl mx-auto py-24 px-4 grid items-center grid-cols-1 gap-y-16 gap-x-8 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
-        <div>
-            <h2 class="max-w-3xl mx-auto text-4xl tracking-tight font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 via-red-700 to-blue-700 sm:text-4xl">Кухонные гарнитуры на заказ и по стандартным модулям</h2>
-            <p class="mt-4 text-gray-500">Недорогие кухонные гарнитуры по низким ценам. Выбирайте наиболее подходящий под ваши нужды вариант.</p>
-
-            <dl class="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
-                <div class="border-t border-gray-200 pt-4">
-                    <dt class="font-medium text-gray-900">Прямые</dt>
-                    <dd class="mt-2 text-sm text-gray-500">Прямые линейные кухонные гарнитуры во всю стену</dd>
-                </div>
-
-                <div class="border-t border-gray-200 pt-4">
-                    <dt class="font-medium text-gray-900">Угловые</dt>
-                    <dd class="mt-2 text-sm text-gray-500">Угловые кухонные гарнитуры на один или два угла</dd>
-                </div>
-
-                <div class="border-t border-gray-200 pt-4">
-                    <dt class="font-medium text-gray-900">Встроенные</dt>
-                    <dd class="mt-2 text-sm text-gray-500">Встроенные кухонные гарнитуры от пола до потолка</dd>
-                </div>
-
-                <div class="border-t border-gray-200 pt-4">
-                    <dt class="font-medium text-gray-900">Модульные</dt>
-                    <dd class="mt-2 text-sm text-gray-500">Модули кухонных гарнитуров, которые вам нужны</dd>
-                </div>
-
-                <div class="border-t border-gray-200 pt-4">
-                    <dt class="font-medium text-gray-900">Пластик</dt>
-                    <dd class="mt-2 text-sm text-gray-500">Кухонные гарнитуры с фасадами из пластика в профиле</dd>
-                </div>
-
-                <div class="border-t border-gray-200 pt-4">
-                    <dt class="font-medium text-gray-900">ПВХ</dt>
-                    <dd class="mt-2 text-sm text-gray-500">Кухонные гарнитуры с фасадами из МДФ с покрытием ПВХ</dd>
-                </div>
-            </dl>
-        </div>
-        <div class="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-4 lg:gap-6">
-            <img alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
-                 class="bg-gray-100 rounded-lg"
-                 src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg">
-            <img alt="Top down view of walnut card tray with embedded magnets and card groove."
-                 class="bg-gray-100 rounded-lg"
-                 src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg">
-            <img alt="Side of walnut card tray with card groove and recessed card area."
-                 class="bg-gray-100 rounded-lg"
-                 src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg">
-            <img alt="Walnut card tray filled with cards and card angled in dedicated groove."
-                 class="bg-gray-100 rounded-lg"
-                 src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg">
-        </div>
-    </div>
-</div>
-
+<MarketingSectionV5 {...contentMarketingSectionV5}/>
 
 <div class="relative py-16 bg-white overflow-hidden">
     <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
@@ -85,8 +30,7 @@
             <svg class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32" fill="none" height="384"
                  viewBox="0 0 404 384" width="404">
                 <defs>
-                    <pattern height="20" id="f210dbf6-a58d-4871-961e-36d5016a0f49" patternUnits="userSpaceOnUse" width="20" x="0"
-                             y="0">
+                    <pattern height="20" id="f210dbf6-a58d-4871-961e-36d5016a0f49" patternUnits="userSpaceOnUse" width="20" x="0" y="0">
                         <rect class="text-gray-200" fill="currentColor" height="4" width="4" x="0" y="0"/>
                     </pattern>
                 </defs>
