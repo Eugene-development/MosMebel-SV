b784942d7e7d82cd950deef753166e2660a8f7cf
indexpro24@gmail.com <indexpro24@gmail.com>
2022-05-22 21:40:08 +0300

    git init

--- app/src/lib/use/functions/gasp/index.js
+++ app/src/lib/use/functions/gasp/index.js
@@ -9,16 +9,17 @@ const gsapOpacity = (boxParam) => {
 
 const gaspText = (param1, param2) => {
     const tl = gsap.timeline();
-    const duration = 2;
+    const duration = 3;
     tl
         .to("#" + param1, {duration, x: 10, delay: 1})
-        .to("#" + param2, {duration, x: 10})
+        .to("#" + param2, {duration, x: 10, repeat: 3, yoyo: true})
 }
 
 const toBig = (param1) => {
     const tl = gsap.timeline();
     tl
-        .to("#" + param1, {scale: 1.02})
+        .to("#" + param1, {scale: 1.02,  delay: 0.3})
+        .to("#" + param1, {scale: 0.99,  })
 }
 
 export const useGasp = {
